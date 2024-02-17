00e318563db722424feee3dc2617bafef5e08ca4
Jonathan <jonathancruzmarin26@gmail.com>
2024-02-16 20:06:03 -0800

    Validación de usuario

---
+++ proyecto.py
@@ -0,0 +1,12 @@
+# # Validación
+
+usuario = input('Digite si es Cliente o Empleado\n').upper()
+
+if usuario == 'EMPLEADO':
+    print('Empleado')
+elif usuario == 'CLIENTE':
+    print('Cliente')
+else:
+    print('Parametro invalido')
+
+# # Registro de Usuarios
